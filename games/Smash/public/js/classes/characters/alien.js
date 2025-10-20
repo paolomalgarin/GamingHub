c658abb4b45279ec42fd0e93e54c4ffbeedbbbd3
Pallo <paolomalgarin@gmail.com>
2025-10-20 11:05:16 +0200

    Updated jumping attack in Smash

--- games/Smash/public/js/classes/characters/alien.js
+++ games/Smash/public/js/classes/characters/alien.js
@@ -7,10 +7,12 @@ export default class Alien extends Player {
 
     topAttack() {
         this.attack(7, 80, { range: 40, anchor: 'top', width: 44, height: 36, offsetY: -20, offsetX: 0 }, .4);
-        this.jumpsRemaining++;
-        this.jump();
-        this.jumpsRemaining--;
-        this.stunTillGnd = true;  //lo stunna finche non tocca terra
+        if (this.jumpsRemaining <= 0) {
+            this.jumpsRemaining++;
+            this.jump();
+            this.jumpsRemaining--;
+            this.stunTillGnd = true;  //lo stunna finche non tocca terra
+        }
     }
     sweepAttack() {
         this.attack(8, 180, { range: 36, anchor: 'bottom', width: 56, height: 28, offsetY: 15, offsetX: 0 }, .5);
